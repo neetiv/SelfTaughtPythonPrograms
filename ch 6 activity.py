59031f390d8768e240173820b6c0be1f6731dc92
neetiv <67287846+neetiv@users.noreply.github.com>
2020-07-16 21:37:55 -0700

    added+deleted things that needed to be

---
+++ ch 6 activity.py
@@ -0,0 +1,14 @@
+age = -1
+add_val = 2
+for other_age in range (1, 7):
+    age = age + add_val
+    print(age)
+print('''
+
+  ''')
+ingredients = [' ', 'snails', 'leeches', 'gorilla fingernail', 'butterfly eyelash', 'centipede fingers']
+for number in range(0, 6):
+    item = number
+    print('%s   %s'  % (number, ingredients[item]))
+    
+    
