33e3683fd48a697c77772da45f69ccd3753b971d
neetiv <67287846+neetiv@users.noreply.github.com>
2021-05-26 10:36:28 -0700

    Create tkinter random rectangle.py

---
+++ tkinter random rectangle.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500)
+canvas.pack()
+
+def rectangle(width, height):
+    x1 = random.randrange(width)
+    y1 = random.randrange(height)
+    x2 = x1 + random.randrange(width)
+    y2 = y1 + random.randrange(height)
+    canvas.create_rectangle(x1, y1, x2, y2)
+
+rectangle(400,400)
