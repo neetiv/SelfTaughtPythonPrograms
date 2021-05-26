829346c825ebfe7af8ffda9a878b2bd845617f28
neetiv <67287846+neetiv@users.noreply.github.com>
2021-05-26 10:46:02 -0700

    Create colored rectangles.py

---
+++ colored rectangles.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import random
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500)
+canvas.pack()
+
+def rectangle(width, height, fill_color):
+    x1 = random.randrange(width)
+    y1 = random.randrange(height)
+    x2 = x1 + random.randrange(width)
+    y2 = y1 + random.randrange(height)
+    canvas.create_rectangle(x1, y1, x2, y2, fill=fill_color)
+
+
+rectangle(400,400, 'green')
+rectangle(400,400, 'red')
+rectangle(400,400, 'blue')
+rectangle(400,400, 'orange')
+rectangle(400,400, 'yellow')
+rectangle(400,400, 'pink')
+rectangle(400,400, 'purple')
+rectangle(400,400, 'violet')
+rectangle(400,400, 'magenta')
+rectangle(400,400, 'cyan')
+
