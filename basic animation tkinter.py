5b3419983bfb3c6715626163edbf5e7f638a76c8
neetiv <67287846+neetiv@users.noreply.github.com>
2021-05-28 21:13:53 -0700

    Update basic animation tkinter.py

--- basic animation tkinter.py
+++ basic animation tkinter.py
@@ -3,9 +3,16 @@ from tkinter import *
 tk=Tk()
 
 canvas=Canvas(tk, width=400, height=400)
-canvas.create_polygon(10,10,10,60,50,35, fill=magenta)
+canvas.create_polygon(10,10,10,60,50,35, fill='magenta')
+canvas.pack()
 
-for i in range(0,60):
-    canvas.move(1,5,5)
-    tk.update()
-    time.sleep(0.05)
+while True:
+    for i in range(0,60):
+        canvas.move(1,5,5)
+        tk.update()
+        time.sleep(0.03)
+        
+    for i in range(0,60):
+        canvas.move(1,-5,-5)
+        tk.update()
+        time.sleep(0.03)
